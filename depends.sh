51c322248c0e8f2a0f258aa2eb58bc7bf975146e
Evgenii Dmitriev <36560997+aguezix@users.noreply.github.com>
2022-08-08 11:42:29 +0300

    Add files via upload

---
+++ depends.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo yum install pangox-compat.i686 libXt.i686 libXp.i686 libpamtest.i686 GConf2-devel.i686 ORBit2-devel.i686 popt-static.i686 gnome-vfs2-devel.i686 libXScrnSaver.i686 libpng12.i686 libart_lgpl.i686 atk.i686 libbonobo.i686 libbonoboui.i686 libxkbfile.i686 libxcrypt-compat.i686 libgnomeui.i686
